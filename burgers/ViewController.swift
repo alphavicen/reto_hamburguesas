cab0cc87152586eff6cc2f527b1c594d715e9b7e
Vicente Alvarez <vamaestre@gmail.com>
2016-06-04 20:58:48 +0200

    last update 20:58

--- burgers/ViewController.swift
+++ burgers/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hamburguesa: UILabel!
+    let burgers = listadoBurgers()
+    
+    @IBOutlet weak var pais: UILabel!
+    let paises = listadoPaises()
+    let colores = listadoColores()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func dameUnaBurger() {
+        hamburguesa.text = burgers.regresaHamburguesa()
+        pais.text = paises.regresaPais()
+        let colorAleatorio = colores.regresaColorAleatorio()
+        view.backgroundColor = colorAleatorio
+        view.tintColor = colorAleatorio
+    }
 
 }
 
